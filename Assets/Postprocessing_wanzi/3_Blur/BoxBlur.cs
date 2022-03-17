69b126a41436235de091fb879dcbb395a7e17060
wangeer <1422375763@qq.com>
2022-03-17 13:31:12 +0800

    1111

---
+++ Assets/Postprocessing_wanzi/3_Blur/BoxBlur.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode()]   //可预览
+public class BoxBlur : MonoBehaviour   //命名必须与c#文件名一致
+{
+    public Material material;   //输入材质球
+    [Range(1,15)]
+    public float _BlurRadius = 5.0f;  //模糊半径
+    [Range(0,10)]
+    public int _Iteration = 4;        //迭代次数
+    [Range(1,10)]
+    public float _DownSample = 2.0f;  //降采样值
+    //public int _BlurOffset = 1;     //迭代偏移量（已弃用）
+
+    //初始化判断,脚本仅运行一次
+    void Start()
+    {
+        if (material == null || material.shader == null)    //SystemInFo.SupportsImageEffects == false
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnRenderImage(RenderTexture soure,RenderTexture destination)
+    {
+        int width1 = (int)(soure.width/_DownSample);          //设置宽高为画布宽高(降采样)
+        int height1 = (int)(soure.height/_DownSample);        //设置宽高为画布宽高(降采样)
+        RenderTexture RT1 = RenderTexture.GetTemporary(width1,height1);   //创建纹理1
+        RenderTexture RT2 = RenderTexture.GetTemporary(width1,height1);   //创建纹理2
+
+        Graphics.Blit(soure,RT1);  //调用纹理
+        material.SetVector("_BlurOffset",new Vector4(_BlurRadius/width1,_BlurRadius/height1,0,0));   //调用shader参数
+
+        for ( int i = 0 ; i < _Iteration ; i++ )
+        {
+            Graphics.Blit(RT1,RT2,material,0);   
+            Graphics.Blit(RT2,RT1,material,0); 
+        }  
+
+        Graphics.Blit(RT1,destination);   //x:输入纹理；y：输出纹理；z：调用材质球中的shader；w：调用shader中的第几个pass，默认为第一个（0）
+
+        //释放已使用过的临时图片
+        RenderTexture.ReleaseTemporary(RT1);
+        RenderTexture.ReleaseTemporary(RT2);
+    }
+}
---
+++ Assets/Postprocessing_wanzi/3_Blur/BoxBlur.shader
@@ -0,0 +1,44 @@
+﻿Shader "Hidden/wanzi/BoxBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _BlurOffset ("模糊强度",Float) = 1
+    }
+    SubShader
+    {
+        
+        Cull Off       //剔除关闭
+        ZWrite off     //深度写入关闭
+        ZTest Always   //深度测试永远通过
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img   
+            //_img可以只使用片元shader
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+
+            sampler2D _MainTex;
+            //float4 _MainTex_TexelSize;   //x=1/width; y=1/height; w=height;   //方法1
+            //float _BlurOffset;   //方法1
+            float4 _BlurOffset;   
+
+            half4 frag (v2f_img i) : SV_Target   //注意v2f_img
+            {
+                //half4 d = _MainTex_TexelSize.xyxy * half4(-1,-1,1,1) * _BlurOffset;   //方法1
+                half4 d = _BlurOffset.xyxy * half4(-1,-1,1,1);
+                half4 s = 0;
+                s += tex2D(_MainTex, i.uv + d.xy);
+                s += tex2D(_MainTex, i.uv + d.zy);
+                s += tex2D(_MainTex, i.uv + d.xw);
+                s += tex2D(_MainTex, i.uv + d.zw);
+                s *= 0.25;
+                return s;
+            }
+            ENDCG
+        }
+    }
+}
