7542c8ce53483d308071f6d0455d52387d8e13d2
wangeer <75481126+wanziwanzi20@users.noreply.github.com>
2022-03-19 20:28:14 +0800

    1111

---
+++ Assets/Postprocessing_wanzi/4_HDRBloom/CutsomBloom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode()]   //可预览
+public class CutsomBloom : MonoBehaviour   //命名必须与c#文件名一致
+{
+    public Material material;   //输入材质球
+    [Range(1,15)]
+    public float _Threshold = 1.0f;  //阈值
+
+    //初始化判断,脚本仅运行一次
+    void Start()
+    {
+        if (material == null || material.shader == null)    //SystemInFo.SupportsImageEffects == false
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+    void OnRenderImage(RenderTexture soure,RenderTexture destination)
+    {
+        int width1 = soure.width/2;          //设置宽高为画布宽高(降采样)
+        int height1 = soure.height/2;        //设置宽高为画布宽高(降采样)
+        RenderTexture RT1 = RenderTexture.GetTemporary(width1,height1);   //创建纹理1
+        RenderTexture RT2 = RenderTexture.GetTemporary(width1,height1);   //创建纹理2
+        material.SetFloat("_Threshold",_Threshold);
+        Graphics.Blit(RT1,destination,material,0);   //阈值
+
+        //模糊
+
+    }
+}
---
+++ Assets/Postprocessing_wanzi/4_HDRBloom/CutsomBloom.shader
@@ -0,0 +1,44 @@
+﻿Shader "Hidden/wanzi/CutsomBloom"
+{
+    CGINCLUDE
+    //自定义库
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+    float _Threshold;
+
+    half4 frag_CutsomBloom (v2f_img i) : SV_Target   //注意v2f_img
+    {
+        half4 Mincol = tex2D(_MainTex, i.uv);
+        half HighCol = max(max(Mincol.r,Mincol.g),Mincol.b);
+        HighCol = max(0.0,(HighCol-_Threshold))/max(HighCol,0.000001);
+        Mincol.rgb *= HighCol;
+        return Mincol;
+    }
+
+    //结束自定义库
+    ENDCG
+
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Threshold ("阈值",Float) = 1
+    }
+    SubShader
+    {
+        
+        Cull Off       //剔除关闭
+        ZWrite off     //深度写入关闭
+        ZTest Always   //深度测试永远通过
+
+        Pass    //pass0
+        {
+            CGPROGRAM
+            //_img可以只使用片元shader
+            #pragma vertex vert_img   
+            //调用自建库中的frag_BoxFilter_4Tap片元着色器
+            #pragma fragment frag_CutsomBloom
+            ENDCG
+        }
+    }
+}
