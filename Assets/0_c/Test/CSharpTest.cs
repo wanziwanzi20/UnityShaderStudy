7ece39ea07b40c917d59d2b6101953c5ed01d397
wangeer <1422375763@qq.com>
2022-04-02 18:57:33 +0800

    1111

---
+++ Assets/0_c/Test/CSharpTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpTest : MonoBehaviour   //public作用是可以让其被访问，表示是公开的
+{
+    void Start()   //void代表没有返回值的方法
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
